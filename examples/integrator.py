4f93216b7e427e9c4ea2d3eb114987e92e923d50
drasmuss <drasmuss@uwaterloo.ca>
2013-11-13 15:40:06 -0500

    Cleaned up ipython notebooks

--- examples/integrator.py
+++ examples/integrator.py
@@ -23,28 +23,32 @@ Network behaviour:
 
 """
 import nengo
+import nengo.helpers
 
-from nengo.objects import Node, Ensemble, Connection
+from nengo.objects import Node, Ensemble, DecodedConnection, Connection, Probe
 
-model = nengo.Model('Integrator')
+model = nengo.Model(label='Integrator')
 
-# Our ensemble consists of 100 leaky integrate-and-fire neurons,
-# representing a one-dimensional signal
-A = Ensemble('A', nengo.LIF(100), dimensions=1)
-print "test"
+with model:
+    # Our ensemble consists of 100 leaky integrate-and-fire neurons,
+    # representing a one-dimensional signal
+    A = Ensemble(nengo.LIF(100), dimensions=1, label='Integrator')
+    
+    # Create a piecewise step function for input
+    input = Node(nengo.helpers.piecewise({0:0,0.2:1,1:0,2:-2,3:0,4:1,5:0}), label='Piecewise input')
+    
+    # Connect the population to itself
+    tau = 0.1
+    DecodedConnection(A, A, transform=[[1]], filter=tau) #The same time constant as recurrent to make it more 'ideal'
+    
+    # Connect the input
+    Connection(input, A, transform=[[tau]], filter=tau) #The same time constant as recurrent to make it more 'ideal'
+    
+    # Add probes
+    p1 = Probe(input, 'output')
+    p2 = Probe(A, 'decoded_output', filter=0.01)
+    p3 = Probe(model.t, 'output')
 
-# Create a piecewise step function for input
-input = Node('Piecewise input', nengo.helpers.piecewise({0:0,0.2:1,1:0,2:-2,3:0,4:1,5:0}))
-
-# Connect the population to itself
-tau = 0.1
-Connection(A, A, transform=[[1]], filter=tau) #The same time constant as recurrent to make it more 'ideal'
-
-# Connect the input
-model.connect(input, A, transform=[[tau]], filter=tau) #The same time constant as recurrent to make it more 'ideal'
-
-model.probe('Piecewise input')
-model.probe('Integrator', filter=0.01) #10ms filter
 
 # Create our simulator
 sim = model.simulator()
@@ -55,7 +59,9 @@ sim.run(6)
 import matplotlib.pyplot as plt
 
 # Plot the decoded output of the ensemble
-t = sim.data(model.t) #Get the time steps
-plt.plot(t, sim.data('A'), label="A output")
-plt.plot(t, sim.data('Input'), 'k', label="Input")
+t = sim.data(p3) #Get the time steps
+plt.plot(t, sim.data(p1), label="Integrator output")
+plt.plot(t, sim.data(p2), 'k', label="Input")
 plt.legend()
+
+plt.show()
